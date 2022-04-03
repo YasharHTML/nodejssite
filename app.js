2ad6a77d3a3151bc8cc4b964eb2837c5223035d2
YasharHTML <85059801+YasharHTML@users.noreply.github.com>
2022-04-03 18:03:01 +0400

    New register and login pages

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
 const app = express();
 app.set('view engine', 'ejs');
 app.use("/html", express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-
+app.use(express.json());
 var frontenddata = [];
 
 app.get("/", async(req, res) => {
@@ -38,7 +39,7 @@ app.get("/post", (req, res) => {
     res.render("post");
 });
 
-app.post("/posted", (req, res) => {
+app.post("/posted", async(req, res) => {
     const data = req.body;
     (async() => {
         const db = await open({
@@ -48,6 +49,27 @@ app.post("/posted", (req, res) => {
         db.run('INSERT INTO data (TITLE, textdata, imglink) VALUES (?, ?, ?)', data.title, data.textdata, data.imglink);
     })()
     res.redirect("/")
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", async(req, res) => {
+    console.log(req.body);
+    const data = req.body;
+    (async() => {
+        const db = await open({
+            filename: 'myDB.db',
+            driver: sqlite3.Database
+        })
+        db.run('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', data.username, data.email, data.password);
+    })()
+    res.end();
 });
 
 app.listen(3000, () => {
